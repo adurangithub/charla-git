08e0b1f21e5a66fec91bf0d7b983ec406119871f
AMDR <i92duroa@uco.es>
2018-10-04 17:25:53 +0200

    Revert "asd"
    This reverts commit b4fd1f3540cfdadd9faf672647ae874ae45e66c3.

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(int argc, char* argv[]){
-  //Agregados parametros por línea de cmandos
+  //Agregados parametros por línea de comandos
   if (argc < 2){
     std::cout<<"Usage: hello Nombre"<<std::endl;
   }
